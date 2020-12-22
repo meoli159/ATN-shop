c0a03f32a63103a274586eff9bc04e14ea3187bf
meoli159 <74394657+meoli159@users.noreply.github.com>
2020-12-22 21:50:39 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ var sessOptions = {
    
 if (app.get('env') === 'production') {
     app.set('trust proxy', 1); // trust first proxy
-    sess.cookie.secure = true; // serve secure cookies
+    session.cookie.secure = true; // serve secure cookies
 }
    
 app.use(session( sessOptions ));
